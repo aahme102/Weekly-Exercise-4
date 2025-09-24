28b965e7eebbf4ce97c83bae3cdb3adb8e954fb8
Anous Ahmed <anous.ahmed102@gmail.com>
2025-09-24 11:57:17 -0500

    Comitting for R Script

---
+++ WEEKLYEXERCISE4.R
@@ -0,0 +1,4 @@
+# 4. Create a new R Script “WEEKLYEXERCISE4” and check the name of the default branch.Report the name as a comment in the R Script. [Hint: install and load the relevant package] (6pts)
+
+The name of the Default Branch is "main"
+
