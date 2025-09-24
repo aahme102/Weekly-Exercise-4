c880e5139b1e11bb7050231a6d84015f2aa62a7d
Anous Ahmed <anous.ahmed102@gmail.com>
2025-09-24 12:04:40 -0500

    Committing Again

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -2,3 +2,18 @@
 
 The name of the Default Branch is "main"
 
+# Ran git2r as it provides access to repositories
+install.packages("git2r")
+library(git2r)
+
+# Asked Chatgpt for clarification on the steps below:
+repo = repository(".")
+
+
+name_branch = repository_head(repo)$name
+
+name_branch
+
+library(readr)
+library(tidyverse)
+library(ggplot2)
